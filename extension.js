9ccf019507482b2119654584e2cd6236ceacf56e
CReviere <creviere12@gmail.com>
2018-11-27 09:07:00 -0600

    Hard-code output device options and invoke amixer wrapper script on user selection

--- extension.js
+++ extension.js
@@ -1,66 +1,38 @@
 const Lang = imports.lang;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
+const Util = imports.misc.util;
 
-const AudioOutputSubMenu = new Lang.Class({
+const switchAudioSource = "/home/cr/Scripts/switchAudioSource"
+
+const AudioOutputSubMenu = new Lang.Class ({
 	Name: 'AudioOutputSubMenu',
 	Extends: PopupMenu.PopupSubMenuMenuItem,
 
 	_init: function () {
-		this.parent('Audio Output: Connecting...', true);
-
-		this._control = Main.panel.statusArea.aggregateMenu._volume._control;
-
-		this._controlSignal = this._control.connect('default-sink-changed', Lang.bind(this, function () {
-			this._updateDefaultSink();
-		}));
-
-		this._updateDefaultSink();
-
-		this.menu.connect('open-state-changed', Lang.bind(this, function (menu, isOpen) {
-			if (isOpen)
-				this._updateSinkList();
-		}));
-
-		//Unless there is at least one item here, no 'open' will be emitted...
-		let item = new PopupMenu.PopupMenuItem('Connecting...');
-		this.menu.addMenuItem(item);
+		this.parent ('Audio Output: Connecting...', true);
+		this.label.set_text ("Select Audio Output Device");
+		this._listOutputDevices();
 	},
 
-	_updateDefaultSink: function () {
-		let defsink = this._control.get_default_sink();
-		//Unfortunately, Gvc neglects some pulse-devices, such as all "Monitor of ..."
-		if (typeof (defsink) === 'undefined' || defsink === null)
-			this.label.set_text('Other');
-		else
-			this.label.set_text(defsink.get_description());
-	},
-
-	_updateSinkList: function () {
+	_listOutputDevices: function () {
 		this.menu.removeAll();
 
-		let defsink = this._control.get_default_sink();
-		let sinklist = this._control.get_sinks();
-		let control = this._control;
+		let headphones = new PopupMenu.PopupMenuItem ('Headphones (Front)');
+		headphones.connect ('activate', Lang.bind (this, function () {
+			Util.spawnCommandLine ("sh " + switchAudioSource + " front");
+		}));
+		this.menu.addMenuItem (headphones);
 
-		for (let i = 0; i < sinklist.length; i++) {
-			let sink = sinklist[i];
-			if (sink === defsink)
-				continue;
-			let item = new PopupMenu.PopupMenuItem(sink.get_description());
-			item.connect('activate', Lang.bind(sink, function () {
-				control.set_default_sink(this);
-			}));
-			this.menu.addMenuItem(item);
-		}
-		if (sinklist.length === 0 || (sinklist.length === 1 && sinklist[0] === defsink)) {
-			let item = new PopupMenu.PopupMenuItem('No More Devices');
-			this.menu.addMenuItem(item);
-		}
+		let speakers = new PopupMenu.PopupMenuItem ('Speakers (Rear)');
+		speakers.connect ('activate', Lang.bind (this, function () {
+			Util.spawnCommandLine ("sh " + switchAudioSource + " back");		
+		}));
+		this.menu.addMenuItem (speakers);
 	},
 
 	destroy: function () {
-		this._control.disconnect(this._controlSignal);
+		this._control.disconnect (this._controlSignal);
 		this.parent();
 	}
 });
@@ -75,7 +47,7 @@ function enable() {
 		return;
 	audioOutputSubMenu = new AudioOutputSubMenu();
 
-	//Try to add the output-switcher right below the output slider...
+	// Try to add the output-switcher right below the output slider...
 	let volMen = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
 	let items = volMen._getMenuItems();
 	let i = 0;
@@ -84,7 +56,7 @@ function enable() {
 			break;
 		else
 			i++;
-	volMen.addMenuItem(audioOutputSubMenu, i + 1);
+	volMen.addMenuItem (audioOutputSubMenu, i + 1);
 }
 
 function disable() {
